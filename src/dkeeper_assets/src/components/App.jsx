813ab6c7504ddd994820c096b34fe92b72234678
sphejiahuan <iamtony_zhou@yahoo.com>
2023-02-27 06:41:43 -0600

    first

---
+++ src/dkeeper_assets/src/components/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import { dkeeper } from "../../../declarations/dkeeper";
+
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    
+    setNotes(prevNotes => {
+      dkeeper.createNote(newNote.title,newNote.content);
+      return [newNote ,  ...prevNotes ];
+    });
+  }
+
+  useEffect( () => {
+    console.log("useeffet is triggered");
+    fetchData();
+  }, []);
+
+  async function fetchData(){
+    const notesArray =await dkeeper.readNotes();
+    setNotes(notesArray);
+  }
+
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      dkeeper.removeNote(id);
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
